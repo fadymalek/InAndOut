e960ea754c8b22f637603b8b3703dc33ac62039c
Fady Malek <fady.malek@pua.local>
2026-03-03 09:23:16 +0200

    read enve directly

--- Program.cs
+++ Program.cs
@@ -3,7 +3,7 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-var conn = builder.Configuration.GetConnectionString("DefaultConnection");
+var conn = Environment.GetEnvironmentVariable("ConnectionStrings__DefaultConnection");
 Console.WriteLine("CONN: " + conn);
 
 builder.Services.AddControllersWithViews();
